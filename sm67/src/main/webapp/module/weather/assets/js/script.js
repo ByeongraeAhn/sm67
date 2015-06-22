3b7ecfdd488f9c8f2d9e37e8056acb9748db635d
burn1985kr <burn1985kr@naver.com>
2015-06-22 16:32:16 +0900

    ..

--- sm67/src/main/webapp/module/weather/assets/js/script.js
+++ sm67/src/main/webapp/module/weather/assets/js/script.js
@@ -91,7 +91,7 @@ $(function(){
 	function addWeather(icon, day, condition){
 
 		var markup = '<li>'+
-			'<img src="assets/img/icons/'+ icon +'.png" />'+
+			'<img src="../module/weather/assets/img/icons/'+ icon +'.png" />'+
 			' <p class="day">'+ day +'</p> <p class="cond">'+ condition +
 			'</p></li>';
 
