09254eaf35d77a7cd0d5bb9f872d2c9ed9d0045e
burn1985kr <burn1985kr@naver.com>
2015-07-03 15:02:39 +0900

    .

--- sm67/src/main/webapp/js/weather.js
+++ sm67/src/main/webapp/js/weather.js
@@ -1,174 +1,178 @@
 $(function(){
 
-    /* Configuration */
+	/* Configuration */
 
-    var DEG = 'c';  // c for celsius, f for fahrenheit
+	var DEG = 'c';			// c for celsius, f for fahrenheit
 
-    var weatherDiv = $('#weather'),
-        scroller = $('#scroller'),
-        location = $('p.location');
-    	var wt_analyze = $('#wt_analyze');
+	var weatherDiv = $('#weather'),
+		scroller = $('#scroller'),
+		location = $('p.location');
 
-    // Does this browser support geolocation?
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
-    }
-    else{
-        showError("Your browser does not support Geolocation!");
-    }
+	// Does this browser support geolocation?
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
+	}
+	else{
+		showError("Your browser does not support Geolocation!");
+	}
 
-    // Get user's location, and use OpenWeatherMap
-    // to get the location name and weather forecast
+	// Get user's location, and use OpenWeatherMap
+	// to get the location name and weather forecast
 
-    function locationSuccess(position) {
+	function locationSuccess(position) {
 
-        try{
+		try{
 
-            // Retrive the cache
-            var cache = localStorage.weatherCache && JSON.parse(localStorage.weatherCache);
+			// Retrive the cache
+			var cache = localStorage.weatherCache && JSON.parse(localStorage.weatherCache);
 
-            var d = new Date();
+			var d = new Date();
 
-            // If the cache is newer than 30 minutes, use the cache
-            if(cache && cache.timestamp && cache.timestamp > d.getTime() - 30*60*1000){
+			// If the cache is newer than 30 minutes, use the cache
+			if(cache && cache.timestamp && cache.timestamp > d.getTime() - 30*60*1000){
 
-                // Get the offset from UTC (turn the offset minutes into ms)
-                var offset = d.getTimezoneOffset()*60*1000;
-                var city = cache.data.city.name;
-                var country = cache.data.city.country;
-                var i=1;
+				// Get the offset from UTC (turn the offset minutes into ms)
+				var offset = d.getTimezoneOffset()*60*1000;
+				var city = cache.data.city.name;
+				var country = cache.data.city.country;
 
-                $.each(cache.data.list, function(){
-                    // "this" holds a forecast object
-                    // Get the local time of this forecast (the api returns it in utc)
-                    var localTime = new Date(this.dt*1000 - offset);
+				$.each(cache.data.list, function(){
+					// "this" holds a forecast object
 
-                    addWeather(
-                        this.weather[0].icon,
-                        moment(localTime).calendar(),   // We are using the moment.js library to format the date
-                        this.weather[0].main + ' <b>' + convertTemperature(this.main.temp_min) + '°' + DEG +
-                                                ' / ' + convertTemperature(this.main.temp_max) + '°' + DEG+'</b>'
-                    );
-                });
-                weatherDiv.addClass('loaded');
+					// Get the local time of this forecast (the api returns it in utc)
+					var localTime = new Date(this.dt*1000 - offset);
 
-                // Set the slider to the first slide
-                
-                showSlide(0);
-            	wt_analyze.append("오늘은 날씨가 별로네요. <br> 야외 활동하기에는 큰 무리가 없을 것 같네요. <br>");
-                
+					addWeather(
+						this.weather[0].icon,
+						moment(localTime).calendar(),	// We are using the moment.js library to format the date
+						this.weather[0].main + ' <b>' + convertTemperature(this.main.temp_min) + '°' + DEG +
+												' / ' + convertTemperature(this.main.temp_max) + '°' + DEG+'</b>'
+					);
 
-            }
+				});
 
-            else{
+				// Add the location to the page
+				location.html(city+', <b>'+country+'</b>');
 
-                // If the cache is old or nonexistent, issue a new AJAX request
+				weatherDiv.addClass('loaded');
 
-                var weatherAPI = 'http://api.openweathermap.org/data/2.5/forecast?lat='+position.coords.latitude+
-                                    '&lon='+position.coords.longitude+'&callback=?'
+				// Set the slider to the first slide
+				showSlide(0);
 
-                $.getJSON(weatherAPI, function(response){
+			}
 
-                    // Store the cache
-                    localStorage.weatherCache = JSON.stringify({
-                        timestamp:(new Date()).getTime(),   // getTime() returns milliseconds
-                        data: response
-                    });
+			else{
+			
+				// If the cache is old or nonexistent, issue a new AJAX request
 
-                    // Call the function again
-                    locationSuccess(position);
-                });
-            }
+				var weatherAPI = 'http://api.openweathermap.org/data/2.5/forecast?lat='+position.coords.latitude+
+									'&lon='+position.coords.longitude+'&callback=?'
 
-        }
-        catch(e){
-            showError("We can't find information about your city!");
-            window.console && console.error(e);
-        }
-    }
+				$.getJSON(weatherAPI, function(response){
 
-    function addWeather(icon, day, condition){
+					// Store the cache
+					localStorage.weatherCache = JSON.stringify({
+						timestamp:(new Date()).getTime(),	// getTime() returns milliseconds
+						data: response
+					});
 
-        var markup = '<li>'+
-            '<img src="../module/weather/assets/img/icons/'+ icon +'.png" />'+ '</li>';
-        scroller.append(markup);
-        
-    }
+					// Call the function again
+					locationSuccess(position);
+				});
+			}
 
-    /* Handling the previous / next arrows */
+		}
+		catch(e){
+			showError("We can't find information about your city!");
+			window.console && console.error(e);
+		}
+	}
 
-    var currentSlide = 0;
-    weatherDiv.find('a.previous').click(function(e){
-        e.preventDefault();
-        showSlide(currentSlide-1);
-    });
+	function addWeather(icon, day, condition){
 
-    weatherDiv.find('a.next').click(function(e){
-        e.preventDefault();
-        showSlide(currentSlide+1);
-    });
+		var markup = '<li>'+
+			'<img src="../module/weather/assets/img/icons/'+ icon +'.png" />'+
+			' <p class="day">'+ day +'</p> <p class="cond">'+ condition +
+			'</p></li>';
 
-    // listen for arrow keys
+		scroller.append(markup);
+	}
 
-    $(document).keydown(function(e){
-        switch(e.keyCode){
-            case 37: 
-                weatherDiv.find('a.previous').click();
-            break;
-            case 39:
-                weatherDiv.find('a.next').click();
-            break;
-        }
-    });
+	/* Handling the previous / next arrows */
 
-    function showSlide(i){
-        var items = scroller.find('li');
+	var currentSlide = 0;
+	weatherDiv.find('a.previous').click(function(e){
+		e.preventDefault();
+		showSlide(currentSlide-1);
+	});
 
-        if (i >= items.length || i < 0 || scroller.is(':animated')){
-            return false;
-        }
+	weatherDiv.find('a.next').click(function(e){
+		e.preventDefault();
+		showSlide(currentSlide+1);
+	});
 
-        weatherDiv.removeClass('first last');
 
-        if(i == 0){
-            weatherDiv.addClass('first');
-        }
-        else if (i == items.length-1){
-            weatherDiv.addClass('last');
-        }
+	// listen for arrow keys
 
-        scroller.animate({left:(-i*100)+'%'}, function(){
-            currentSlide = i;
-        });
-    }
+	$(document).keydown(function(e){
+		switch(e.keyCode){
+			case 37: 
+				weatherDiv.find('a.previous').click();
+			break;
+			case 39:
+				weatherDiv.find('a.next').click();
+			break;
+		}
+	});
 
-    /* Error handling functions */
+	function showSlide(i){
+		var items = scroller.find('li');
 
-    function locationError(error){
-        switch(error.code) {
-            case error.TIMEOUT:
-                showError("A timeout occured! Please try again!");
-                break;
-            case error.POSITION_UNAVAILABLE:
-                showError('We can\'t detect your location. Sorry!');
-                break;
-            case error.PERMISSION_DENIED:
-                showError('Please allow geolocation access for this to work.');
-                break;
-            case error.UNKNOWN_ERROR:
-                showError('An unknown error occured!');
-                break;
-        }
+		if (i >= items.length || i < 0 || scroller.is(':animated')){
+			return false;
+		}
 
-    }
+		weatherDiv.removeClass('first last');
 
-    function convertTemperature(kelvin){
-        // Convert the temperature to either Celsius or Fahrenheit:
-        return Math.round(DEG == 'c' ? (kelvin - 273.15) : (kelvin*9/5 - 459.67));
-    }
+		if(i == 0){
+			weatherDiv.addClass('first');
+		}
+		else if (i == items.length-1){
+			weatherDiv.addClass('last');
+		}
 
-    function showError(msg){
-        weatherDiv.addClass('error').html(msg);
-    }
+		scroller.animate({left:(-i*100)+'%'}, function(){
+			currentSlide = i;
+		});
+	}
+
+	/* Error handling functions */
+
+	function locationError(error){
+		switch(error.code) {
+			case error.TIMEOUT:
+				showError("A timeout occured! Please try again!");
+				break;
+			case error.POSITION_UNAVAILABLE:
+				showError('We can\'t detect your location. Sorry!');
+				break;
+			case error.PERMISSION_DENIED:
+				showError('Please allow geolocation access for this to work.');
+				break;
+			case error.UNKNOWN_ERROR:
+				showError('An unknown error occured!');
+				break;
+		}
+
+	}
+
+	function convertTemperature(kelvin){
+		// Convert the temperature to either Celsius or Fahrenheit:
+		return Math.round(DEG == 'c' ? (kelvin - 273.15) : (kelvin*9/5 - 459.67));
+	}
+
+	function showError(msg){
+		weatherDiv.addClass('error').html(msg);
+	}
 
 });
